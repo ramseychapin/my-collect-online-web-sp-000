28cfa545a419c394a67a36cf6680079a1410c4b9
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-25 03:13:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  i = 0;
+  result_collection = []
+  while i < collection.length
+    result_collection << yield(collection[i])
+    i += 1
+  end
+  result_collection
+end
